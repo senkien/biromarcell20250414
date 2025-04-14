7b2e67f684fb3ec1c19edae00a9d5a51d500bf9c
senkien <157018483+senkien@users.noreply.github.com>
2025-04-14 16:16:39 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-// src/app/app-routing.module.ts
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RecipesComponent } from './recipes/recipes.component';
