52905529dfd97b7bbe13fc041dc03783e926363f
senkien <157018483+senkien@users.noreply.github.com>
2025-04-14 16:16:18 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
+// src/app/app-routing.module.ts
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ReadRecipeComponent } from './read-recipe/read-recipe.component';
+import { UpdateRecipeComponent } from './update-recipe/update-recipe.component';
+import { RecipeResolver } from '../services/recipe.resolver';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'recipes', pathMatch: 'full' },
+  {
+    path: 'recipes',
+    component: RecipesComponent,
+    children: [
+      {
+        path: 'read/:id',
+        component: ReadRecipeComponent,
+        resolve: { recipe: RecipeResolver },
+      },
+      {
+        path: 'update/:id',
+        component: UpdateRecipeComponent,
+        resolve: { recipe: RecipeResolver },
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+
